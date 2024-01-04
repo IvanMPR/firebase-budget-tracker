d434eacd2d61501a6489963484b7365744f708f2
IvanMPR <ivsanmas@gmail.com>
2024-01-04 22:58:58 +0100

    dev

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
             className=' flex flex-col p-4 items-left justify-center'
           >
             <p className=' text-stone-700 text-5xl w-2/3 leading-[4rem] mb-8'>
-              Keep track of your finances with our awesome app!
+              Keep track of your finances with this awesome app!
             </p>
             <Link to='/signup'>
               <button className='inline-block rounded-full w-40 bg-stone-700  font-semibold uppercase tracking-wide text-yellow-400 transition-colors hover:bg-stone-800 active:translate-y-[2px] disabled:cursor-not-allowed disabled:opacity-50 sm:px-6 sm:py-4 px-4 py-2 md:px-5 md:py-2.5 text-xs'>
