9a5adfb2cb2c269864f22b295470e4c7d7e731e4
IvanMPR <ivsanmas@gmail.com>
2024-02-14 23:37:35 +0100

    Update border style in Amount component

--- src/app-components/Amount.jsx
+++ src/app-components/Amount.jsx
@@ -1,7 +1,7 @@
 function Amount({ bgColor, children }) {
   return (
     <div
-      className={` ${bgColor} border-y-4 border-slate-700   uppercase flex justify-between items-center my-4 m-auto w-96 py-2 px-4 }`}
+      className={` ${bgColor} border-y-2 border-stone-700   uppercase flex justify-between items-center my-4 m-auto w-96 py-2 px-4 }`}
     >
       {children}
     </div>
