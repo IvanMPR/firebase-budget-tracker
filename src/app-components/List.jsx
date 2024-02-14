b44128783573777deafa9be77ca84e55ffcbfc09
IvanMPR <ivsanmas@gmail.com>
2024-02-14 23:37:57 +0100

    Update List component styling

--- src/app-components/List.jsx
+++ src/app-components/List.jsx
@@ -2,7 +2,7 @@ import ListItem from "./ListItem";
 
 function List({ type, entriesType }) {
   return (
-    <div className='w-full [&>*:last-child]:mb-8'>
+    <div className='w-full [&>*:last-child]:mb-4 max-h-[45vh] overflow-y-auto'>
       <ul className={`list ${type}`}>
         {entriesType.map(entry => (
           <ListItem entry={entry} key={entry.id} />
