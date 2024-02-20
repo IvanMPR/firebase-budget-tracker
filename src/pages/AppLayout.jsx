a1fb024dadb775db44da6b1ad4d69ab863c85be6
IvanMPR <ivsanmas@gmail.com>
2024-02-20 22:46:07 +0100

    Add AppLayout component

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,31 @@
+import Header from "../components/Header";
+import Amounts from "../app-components/Amounts";
+import Inputs from "../app-components/Inputs";
+import ListsParent from "../app-components/ListsParent";
+import { useAuthContext } from "../contexts/AuthContext";
+import { useEffect } from "react";
+
+function BudgetTracker() {
+  const { logOut } = useAuthContext();
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", logOut);
+
+    return () => {
+      window.removeEventListener("beforeunload", logOut);
+    };
+  }, [logOut]);
+
+  return (
+    <section>
+      <Header />
+      <div className=' w-screen flex flex-col items-center justify-center'>
+        <Amounts />
+        <Inputs />
+        <ListsParent />
+      </div>
+    </section>
+  );
+}
+
+export default BudgetTracker;
