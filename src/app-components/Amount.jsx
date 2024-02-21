0675a923f00af1f7d62d8b1a2e4ef6813dc6b7ea
IvanMPR <ivsanmas@gmail.com>
2024-02-21 19:31:42 +0100

    Refactor Amount component to include default value for bgColor prop

--- src/app-components/Amount.jsx
+++ src/app-components/Amount.jsx
@@ -1,4 +1,4 @@
-function Amount({ bgColor, children }) {
+function Amount({ bgColor = "", children }) {
   return (
     <div
       className={` ${bgColor} border-y-2 border-stone-700   uppercase flex justify-between items-center my-4 m-auto w-96 py-2 px-4 }`}
