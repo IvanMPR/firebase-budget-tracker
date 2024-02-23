83e4774ff7a860dc1650a6ee0a249d57465cb5ca
IvanMPR <ivsanmas@gmail.com>
2024-02-23 19:51:36 +0100

    Update Header component layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   const { user } = useAuthContext();
 
   return (
-    <header className=' flex p-4 bg-yellow-400 justify-center items-center w-full h-[4.5rem] '>
+    <header className=' flex flex-col  bg-yellow-400 justify-center items-center w-full md:h-[4.5rem] md:flex-row md:justify-between'>
       <ContentWrapper>
         <Logo />
         {user ? <Avatar /> : <Login />}
