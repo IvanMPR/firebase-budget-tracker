f1f783927b4a6a3217e1178bd14dddb2e3e8000a
IvanMPR <ivsanmas@gmail.com>
2024-02-25 22:14:59 +0100

    Add useBudgetTrackerContext hook to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,16 @@ import ContentWrapper from "./ContentWrapper";
 import Login from "../pages/Login";
 
 import { useAuthContext } from "../contexts/AuthContext";
+import { useBudgetTrackerContext } from "../contexts/BudgetTrackerContext";
 
 function Header() {
   const { user } = useAuthContext();
+  const { isSmallScreen } = useBudgetTrackerContext();
 
   return (
-    <header className=' flex flex-col  bg-yellow-400 justify-center items-center w-full md:h-[4.5rem] md:flex-row md:justify-between'>
+    <header className=' flex flex-col py-2  bg-yellow-400 justify-center items-center w-full md:h-[4.5rem] md:flex-row md:justify-between xl:flex-row '>
       <ContentWrapper>
-        <Logo />
+        {isSmallScreen && <Logo />}
         {user ? <Avatar /> : <Login />}
       </ContentWrapper>
     </header>
