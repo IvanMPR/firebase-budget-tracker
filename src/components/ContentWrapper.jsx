e381e1bcd50f91bac0bcc762823f1f1f2cc279b0
IvanMPR <ivsanmas@gmail.com>
2024-02-25 22:14:51 +0100

    Refactor ContentWrapper styling

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -1,6 +1,6 @@
 function ContentWrapper({ children }) {
   return (
-    <div className=' flex  px-2 justify-between items-center w-full md:w-2/3 md:m-auto'>
+    <div className=' flex px-2 justify-between items-center w-full md:w-2/3 md:m-auto'>
       {children}
     </div>
   );
