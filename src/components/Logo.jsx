7b39d628ea15b281b1742a47efca44a1c34a974a
IvanMPR <ivsanmas@gmail.com>
2024-02-25 22:15:03 +0100

    Update Logo component styling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 function Logo() {
   return (
     <div>
-      <h1 className=' uppercase text-normal font-bold tracking-wide mb-1 text-stone-700 md:text-xl md:tracking-widest'>
+      <h1 className=' uppercase text-normal font-bold tracking-wide mb-1 text-stone-700 md:text-l md:tracking-normal xl:text-xl xl:w-40'>
         Budget Tracker
       </h1>
     </div>
