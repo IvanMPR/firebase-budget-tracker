d5ebbb6e79996c0d5d30ab68e43ff488cca1b9c2
IvanMPR <ivsanmas@gmail.com>
2024-02-26 16:17:22 +0100

    Update styling for Amounts component

--- src/app-components/Amounts.jsx
+++ src/app-components/Amounts.jsx
@@ -15,7 +15,7 @@ function Amounts() {
 
   return (
     <ContentWrapper>
-      <div className=' w-full px-4 xl:w-[50%] xl:m-auto'>
+      <div className=' w-full px-4 xl:w-[50%] xl:m-auto xl:justify-center'>
         <div className='xl:w-[80%] xl:m-auto'>
           <Amount ring='ring-3 ring-slate-400 ring-offset-2'>
             <span className='block text-stone-700 font-bold'>
@@ -30,27 +30,27 @@ function Amounts() {
             </h3>
           </Amount>
         </div>
-        <div className=' w-full flex flex-col xl:flex-row justify-between'>
-          <div className='  sm:mr-0 xl:w-[40%]'>
+        <div className='flex flex-col xl:flex-row w-full   justify-between '>
+          <div className='  sm:mr-0 xl:w-[40%] '>
             <Amount ring='ring-3 ring-green-400 ring-offset-2'>
               <span className='block text-slate-700 font-bold'>Income: </span>
               <h3 className='font-bold text-slate-700 text-xl bg-green-200 px-2'>
                 {isFetchingEntries ? (
                   <SmallSpinner />
                 ) : (
-                  `${CURRENCY_SYMBOL} ${roundNumber(incomeFunds)}`
+                  `${CURRENCY_SYMBOL}${roundNumber(incomeFunds)}`
                 )}
               </h3>
             </Amount>
           </div>
-          <div className='  sm:ml-0 xl:w-[40%]'>
+          <div className='  sm:ml-0 xl:w-[40%] '>
             <Amount ring='ring-3 ring-red-400 ring-offset-2'>
               <span className='block text-slate-700 font-bold'>Expense: </span>
               <h3 className='font-bold text-slate-700 text-xl bg-rose-200 px-2'>
                 {isFetchingEntries ? (
                   <SmallSpinner />
                 ) : (
-                  `${CURRENCY_SYMBOL} ${roundNumber(expenseFunds)}`
+                  `${CURRENCY_SYMBOL}${roundNumber(expenseFunds)}`
                 )}
               </h3>
             </Amount>
